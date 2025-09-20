c468e0910033bb5158e7e809c003d69130ffdd63
HELLxCODER <pranavvasisth10@gmail.com>
2025-09-20 13:56:13 +0530

    Intial Commit

---
+++ Task1.py
@@ -0,0 +1,16 @@
+print("Hello User!")
+
+n = int(input("Enter number 1: "))
+n1 = int(input("Enter number 2: "))
+
+addi = n + n1
+sub = n1 - n
+mul = n * n1
+div = n1 / n
+
+print("The values of the operations are:")
+
+print(f"Addition: {addi}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mul}")
+print(f"Division: {div}")
---
+++ Task2.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+Full_name = first_name+" "+last_name
+print(f"Hello, {Full_name} welcome to Python Programming!")
